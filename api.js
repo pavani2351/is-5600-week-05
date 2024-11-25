46c66018f2297592658a5f1a9d61c9829050d811
pavani2351 <pavani.kommanaboina@slu.edu>
2024-11-25 04:04:07 +0000

    lab 5 submission

--- api.js
+++ api.js
@@ -83,3 +83,71 @@ module.exports = autoCatch({
   editProduct,
   deleteProduct
 });
+const express = require('express');
+const Products = require('./products');
+const Orders = require('./orders');
+
+async function createProduct(req, res, next) {
+  const product = await Products.create(req.body);
+  res.json(product);
+}
+
+async function listProducts(req, res, next) {
+  const { offset, limit, tag } = req.query;
+  const products = await Products.list({ offset, limit, tag });
+  res.json(products);
+}
+
+async function getProduct(req, res, next) {
+  const product = await Products.get(req.params.id);
+  res.json(product);
+}
+
+async function editProduct(req, res, next) {
+  const product = await Products.edit(req.params.id, req.body);
+  res.json(product);
+}
+
+async function deleteProduct(req, res, next) {
+  const response = await Products.destroy(req.params.id);
+  res.json(response);
+}
+
+async function createOrder(req, res, next) {
+  const order = await Orders.create(req.body);
+  res.json(order);
+}
+
+async function listOrders(req, res, next) {
+  const { offset, limit, productId, status } = req.query;
+  const orders = await Orders.list({ offset, limit, productId, status });
+  res.json(orders);
+}
+
+async function getOrder(req, res, next) {
+  const order = await Orders.get(req.params.id);
+  res.json(order);
+}
+
+async function editOrder(req, res, next) {
+  const order = await Orders.edit(req.params.id, req.body);
+  res.json(order);
+}
+
+async function deleteOrder(req, res, next) {
+  const response = await Orders.destroy(req.params.id);
+  res.json(response);
+}
+
+module.exports = {
+  createProduct,
+  listProducts,
+  getProduct,
+  editProduct,
+  deleteProduct,
+  createOrder,
+  listOrders,
+  getOrder,
+  editOrder,
+  deleteOrder,
+};
--- app.js
+++ app.js
@@ -22,3 +22,24 @@ app.post('/products', api.createProduct)
 // Boot the server
 app.listen(port, () => console.log(`Server listening on port ${port}`))
 
+const express = require('express');
+const api = require('./api');
+
+const app = express();
+app.use(express.json());
+
+app.post('/products', api.createProduct);
+app.get('/products', api.listProducts);
+app.get('/products/:id', api.getProduct);
+app.put('/products/:id', api.editProduct);
+app.delete('/products/:id', api.deleteProduct);
+
+app.post('/orders', api.createOrder);
+app.get('/orders', api.listOrders);
+app.get('/orders/:id', api.getOrder);
+app.put('/orders/:id', api.editOrder);
+app.delete('/orders/:id', api.deleteOrder);
+
+app.listen(3000, () => {
+  console.log('Server is running on port 3000');
+});
